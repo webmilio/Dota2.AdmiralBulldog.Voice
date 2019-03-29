44f892c4f3c44552cb5f3c18306b4cea1b30c6a7
webmilio <therealwebmilio@gmail.com>
2019-03-29 18:10:04 -0400

    Renamed project.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Webmilio.Dota2.AdmiralBulldog.Properties {
+namespace Webmilio.Dota2.AdmiralBulldog.CustomVoice.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Webmilio.Dota2.AdmiralBulldog.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Webmilio.Dota2.AdmiralBulldog.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Webmilio.Dota2.AdmiralBulldog.CustomVoice.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
